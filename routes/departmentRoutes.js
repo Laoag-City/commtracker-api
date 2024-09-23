a69c42d8b3ac34f4def6650a0b4d1df57b9d0b85
Richard Alhama <richard.alhama@gmail.com>
2024-09-23 06:43:31 +0000

    code cleanup and added crud routes to departments

--- routes/departmentRoutes.js
+++ routes/departmentRoutes.js
@@ -5,8 +5,29 @@ const authenticateJWT = require('../middlewares/authMiddleware');
 
 // GET all departments (protected route)
 router.get('/', authenticateJWT, departmentController.getAllDepartments);
+router.get('/:id', authenticateJWT, departmentController.getDepartmentById); 
 
 // POST create a new department (protected route)
 router.post('/new', authenticateJWT, departmentController.createDepartment);
+//router.post('/', authenticateJWT, departmentController.createDepartment); // Create a new department
+// Update a department by ID
+router.put('/:id', authenticateJWT, departmentController.updateDepartment); 
+router.delete('/:id', authenticateJWT, departmentController.deleteDepartment); 
+router.delete('/:id', authenticateJWT, departmentController.deleteDepartment);
 
 module.exports = router;
+
+/* const express = require('express');
+const router = express.Router();
+const departmentController = require('../controllers/departmentController');
+const authenticateJWT = require('../middlewares/auth');
+
+// CRUD operations for departments
+router.get('/', authenticateJWT, departmentController.getAllDepartments); // Get all departments
+router.get('/:id', authenticateJWT, departmentController.getDepartmentById); // Get a department by ID
+router.post('/', authenticateJWT, departmentController.createDepartment); // Create a new department
+router.put('/:id', authenticateJWT, departmentController.updateDepartment); // Update a department by ID
+router.delete('/:id', authenticateJWT, departmentController.deleteDepartment); // Delete a department by ID
+
+module.exports = router;
+ */
