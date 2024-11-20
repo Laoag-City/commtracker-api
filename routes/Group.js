11cca1ec9f59fe746fcfa2ff66c51b298a45bf45
Richard Alhama <richard.alhama@gmail.com>
2024-11-20 04:32:29 +0000

    working group refactor (filename)

--- routes/Group.js
+++ routes/Group.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const groupController = require('../controllers/groupController');
+const groupController = require('../controllers/Group');
 const authenticateJWT = require('../middlewares/authMiddleware');
 
 // CRUD routes for groups
