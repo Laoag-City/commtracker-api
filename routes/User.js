7bf27cb5382c5eef593cb28e1640fec15643d1e0
Richard Alhama <richard.alhama@gmail.com>
2024-11-26 06:46:40 +0000

    change login limit cap

--- routes/User.js
+++ routes/User.js
@@ -8,7 +8,7 @@ const authenticateJWT = require('../middlewares/authMiddleware');
 // Rate limiter for login route
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // Limit each IP to 5 requests per windowMs
+  max: 60, // Limit each IP to 5 requests per windowMs
   message: { message: 'Too many login attempts from this IP, please try again later.' }, // Return message as JSON
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
