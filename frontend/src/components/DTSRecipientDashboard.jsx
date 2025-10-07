e81e0138be192f92dc14b13d704dfec57d4d73ba
Richard L. Alhama <richard.alhama@gmail.com>
2025-10-07 13:19:48 +0800

    new recipient reply

--- frontend/src/components/DTSRecipientDashboard.jsx
+++ frontend/src/components/DTSRecipientDashboard.jsx
@@ -352,6 +352,8 @@ const DTSRecipientDashboard = () => {
                     <option value="pending">-Pending-</option>
                     <option value="approved">Approved</option>
                     <option value="noted">Noted</option>
+                    <option value="for compliance">For Compliance</option>
+                    <option value="pls facilitate">Please Facilitate</option>
                     <option value="in-progress">In Progress</option>
                     <option value="rejected">Rejected</option>
                   </Form.Select>
